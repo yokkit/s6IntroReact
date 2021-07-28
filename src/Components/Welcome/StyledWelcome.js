a70a0c4cf46c00e80798a9eee582953739fc0e00
yokkit <yoko.tochigi@gmail.com>
2021-07-28 16:36:27 +0200

    Add nivel2

---
+++ src/Components/Welcome/StyledWelcome.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const WelcomeDiv = styled.div`
+  text-align: center;
+  margin: 5% 10%;
+  padding: 3rem;
+  background-color: #F0E4D7;
+  color: #BD4B4B;
+
+  h1 {
+    font-size: 4rem;
+    
+  }
+  p {
+    margin: 5% 10%;
+  }
+  button {
+    width: 150px;
+    height: 50px;
+    background: #FF7171;
+    color: #F0E4D7;
+    font-size:1rem;
+    border-radius: 3px;
+    border: none;
+  }
+`;
