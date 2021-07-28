9d303136299a99702ae0b0fc6e96a9281a078ec9
yokkit <yoko.tochigi@gmail.com>
2021-07-28 16:37:41 +0200

    Add nivel2(App.js)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import Escena from './Components/Escena/Escena';
-import data from './Components/data/data.json';
-
+import React, { useState } from "react";
+import Escena from "./Components/Escena/Escena";
+import data from "./Components/data/data.json";
+import Welcome from "./Components/Welcome/Welcome";
 
 function App() {
+  const [firstAccess, setFirstAccess] = useState(false);
+  const checkAccess = () => {
+    setFirstAccess(true);
+  };
   return (
-    <Escena data={data}/>
+    <div>
+      {!firstAccess ? (
+        <Welcome startStory={checkAccess} />
+      ) : (
+        <Escena data={data} />
+      )}
+    </div>
   );
 }
 
